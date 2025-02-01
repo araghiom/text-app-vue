68215e6599cb802259c36d574ae36648dba946b0
omid <araghiom@gmail.com>
2025-02-02 02:13:03 +0330

    create button base for ui

---
+++ src/components/base/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  id: String,
+  label: String,
+  isLoading: Boolean,
+  isDisabled: Boolean,
+  onClick: Function,
+});
+</script>
+
+<template>
+  <button :id="id" :disabled="isDisabled || isLoading" @click="onClick" class="btn">
+    <span v-if="isLoading">Loading...</span>
+    <span v-else>{{ label }}</span>
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 10px;
+  background: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin: 5px;
+}
+.btn:disabled {
+  background: #ccc;
+  cursor: not-allowed;
+}
+</style>
