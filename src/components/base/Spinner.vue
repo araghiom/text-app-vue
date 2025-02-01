e9fed075ca780ba8aa62896344f259766efee3df
omid <araghiom@gmail.com>
2025-02-02 02:14:03 +0330

    create loading spinner

---
+++ src/components/base/Spinner.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="loading-container">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style scoped>
+.loading-container {
+  text-align: center;
+  font-weight: bold;
+  color: #007bff;
+  margin: 10px 0;
+}
+</style>
